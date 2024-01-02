9acda8efef3e4f0f955eef21aa5e3f08116e7d55
Lorenzo Carvelli <lca@tailwindbiotech.com>
2024-01-02 16:19:48 +0100

    cleanup

--- 2023/19.py
+++ 2023/19.py
@@ -46,31 +46,12 @@ class Node(object):
 class Day19(DailyPuzzle):
     def __init__(self):
         super().__init__("2023", "19")
-        self.mock_data = [
-            "px{a<2006:qkq,m>2090:A,rfg}",
-            "pv{a>1716:R,A}",
-            "lnx{m>1548:A,A}",
-            "rfg{s<537:gd,x>2440:R,A}",
-            "qs{s>3448:A,lnx}",
-            "qkq{x<1416:A,crn}",
-            "crn{x>2662:A,R}",
-            "in{s<1351:px,qqz}",
-            "qqz{s>2770:qs,m<1801:hdj,R}",
-            "gd{a>3333:R,R}",
-            "hdj{m>838:A,pv}",
-            "",
-            "{x=787,m=2655,a=1222,s=2876}",
-            "{x=1679,m=44,a=2067,s=496}",
-            "{x=2036,m=264,a=79,s=2244}",
-            "{x=2461,m=1339,a=466,s=291}",
-            "{x=2127,m=1623,a=2188,s=1013}"
-        ]
 
     def parse_data(self) -> (list, list):
         workflows, parts = dict(), []
         parsing_workflows = True
 
-        for ll in self.data:  # TODO: replace with self.data
+        for ll in self.data:
             if ll == "":
                 parsing_workflows = False
                 continue
