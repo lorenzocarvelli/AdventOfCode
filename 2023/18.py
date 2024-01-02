a4b3ce836b08f2417a6fa6d6cb232a1a30dc700d
Lorenzo Carvelli <lca@tailwindbiotech.com>
2024-01-02 11:26:09 +0100

    day 18 pt2

--- 2023/18.py
+++ 2023/18.py
@@ -6,22 +6,6 @@ from base import DailyPuzzle
 class Day18(DailyPuzzle):
     def __init__(self):
         super().__init__("2023", "18")
-        self.mock_data = [
-            "R 6 (#70c710)",
-            "D 5 (#0dc571)",
-            "L 2 (#5713f0)",
-            "D 2 (#d2c081)",
-            "R 2 (#59c680)",
-            "D 2 (#411b91)",
-            "L 5 (#8ceee2)",
-            "U 2 (#caa173)",
-            "L 1 (#1b58a2)",
-            "U 2 (#caa171)",
-            "R 2 (#7807d2)",
-            "U 3 (#a77fa3)",
-            "L 2 (#015232)",
-            "U 2 (#7a21e3)"
-        ]
 
         self.direction_dict: dict = {
             "U": (-1, 0),
@@ -30,18 +14,71 @@ class Day18(DailyPuzzle):
             "R": (0, 1)
         }
 
+    @staticmethod
+    def solve_pt2(h_lines: list, v_lines: list):
+        # Credits to tm01 - see: https://github.com/tmo1/adventofcode/blob/main/2023/18b.py
+        h_bars = sorted({h_line[2] for h_line in h_lines})
+        v_bars = sorted({v_line[2] for v_line in v_lines})
+        total = 0
+        cells = []
+        for y in range(len(h_bars) - 1):
+            cell_row = []
+            h_bar_prev = h_bars[y]
+            h_bar = h_bars[y + 1]
+            for x in range(len(v_bars) - 1):
+                v_bar_left = False
+                for v_line in v_lines:
+                    if h_bar_prev >= v_line[0] and h_bar <= v_line[1] and v_bars[x] == v_line[2]:
+                        v_bar_left = True
+                        break
+                prev_in = False if (x == 0 or not cell_row[x - 1]) else True
+                if prev_in != v_bar_left:
+                    cell_row.append(True)
+                    total += (h_bar - h_bar_prev - 1) * (v_bars[x + 1] - v_bars[x] - 1)
+                    if prev_in:
+                        total += h_bar - h_bar_prev - 1
+                    if y > 0 and cells[y - 1][x]:
+                        total += v_bars[x + 1] - v_bars[x] - 1
+                    if prev_in and not v_bar_left and y > 0 and cells[y - 1][x] and cells[y - 1][x - 1]:
+                        total += 1
+                else:
+                    cell_row.append(False)
+            cells.append(cell_row)
+        print(total + sum([v_line[1] - v_line[0] + 1 for v_line in v_lines]) + sum(
+            [h_line[1] - h_line[0] + 1 for h_line in h_lines]) - len(v_lines) - len(h_lines))
+
     def solve(self):
 
         current_coordinate = (0, 0)
+        x, y = 0, 0
         trench_coordinates = [current_coordinate]
+        h_lines = []
+        v_lines = []
         for ll in self.data:
             direction = ll[0]
             steps = int(re.match(r".+ (\d+) \(", ll).group(1))
             new_dir = self.direction_dict[direction]
 
+            hex_d = re.match(r".+ \(#(.+)\)$", ll).group(1)
+            steps_pt2 = int(hex_d[:-1], 16)
+            if hex_d[-1] == "0":
+                h_lines.append((x, x + steps_pt2, y))
+                x += steps_pt2
+            if hex_d[-1] == "1":
+                v_lines.append((y, y + steps_pt2, x))
+                y += steps_pt2
+            if hex_d[-1] == "2":
+                h_lines.append((x - steps_pt2, x, y))
+                x -= steps_pt2
+            if hex_d[-1] == "3":
+                v_lines.append((y - steps_pt2, y, x))
+                y -= steps_pt2
+
             for _ in range(steps):
                 current_coordinate = (current_coordinate[0] + new_dir[0], current_coordinate[1] + new_dir[1])
                 trench_coordinates.append(current_coordinate)
+
+        self.solve_pt2(h_lines, v_lines)
 
         filled_trench = self.fill_grid(trench_coordinates)
         p1 = self.draw_grid_and_count_m3_lava(filled_trench)
